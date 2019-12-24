208645af3ef28f3eb19bc2f5db95431c4fda16df
Сергей Ефремов <yefr@MacBook-Pro-Sergej-3.local>
2019-12-24 22:50:38 +0300

    task

--- client.py
+++ client.py
@@ -1,5 +1,20 @@
 import socket
 import pickle
+from random import randint
+
+
+def crypt(a1, key):
+    a2 = [chr(ord(a1[i]) ^ key) for i in range(len(a1))]
+    return ''.join(a2)
+
+def send(sock, msg, K):
+    msg = crypt(msg, K)
+    sock.send(pickle.dumps(msg))
+
+def receive(sock, K):
+    msg = pickle.loads(sock.recv(1024))
+    msg = crypt(msg,K)
+    return msg
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -7,8 +22,17 @@ PORT = 8080
 sock = socket.socket()
 sock.connect((HOST, PORT))
 
-p, g, a = 7, 5, 3
+p, g, a = [randint(0,250) for i in range(3)]
 A = g ** a % p
 sock.send(pickle.dumps((p, g, A)))
+B = pickle.loads(sock.recv(1024))
+K = B ** a % p
+print('Enter your message')
+msg = input()
+while msg != 'exit':
+    send(sock,msg, K)
+    print(receive(sock,K))
+    msg = input()
+
 
 sock.close()
--- server.py
+++ server.py
@@ -1,5 +1,22 @@
 import socket
 import pickle
+from random import randint
+
+def crypt(a1, key):
+    a2 = [chr(ord(a1[i]) ^ key) for i in range(len(a1))]
+    return ''.join(a2)
+
+
+def send(conn, msg, K):
+    msg = crypt(msg, K)
+    conn.send(pickle.dumps(msg))
+
+
+def receive(conn, K):
+    msg = pickle.loads(conn.recv(1024))
+    msg = crypt(msg, K)
+    return msg
+
 
 HOST = '127.0.0.1'
 PORT = 8080
@@ -10,6 +27,22 @@ sock.listen(1)
 conn, addr = sock.accept()
 
 msg = conn.recv(1024)
-print(pickle.loads(msg))
+p, g, A = pickle.loads(msg)
+b = randint(10, 250)
+B = g ** b % p
+conn.send(pickle.dumps(B))
+K = A ** b % p
+
+
+while True:
+    try:
+        msg = receive(conn, K)
+        print(msg)
+        send(conn, msg, K)
+    except EOFError:
+        break
+
+
+
 
 conn.close()
